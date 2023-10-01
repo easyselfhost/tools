0931ba5452d423e531e898b6dd5694a850a026d4
easyselfhost <easyselfhost@gmail.com>
2023-09-30 20:12:41 -0700

    Bump version to 0.0.4.

--- tools/scripts/src/main.ts
+++ tools/scripts/src/main.ts
@@ -2,7 +2,7 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import * as cmds from "./cmds/index.js";
 
-const version = "0.0.2";
+const version = "0.0.4";
 
 yargs(hideBin(process.argv))
   .command(cmds.CheckCommand)
